1ff6a0123d52c38c5e35ab8f8dd6409b25a95e5b
Alexey Panferov <lexavtanke@gmail.com>
2024-02-13 16:41:57 +0300

    feat(test_node): add test_publisher with proper qos param Signed-off-by: Alexey Panferov <lexavtanke@gmail.com>

---
+++ common/test_node/src/test_pub.cpp
@@ -0,0 +1,51 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+    MinimalPublisher()
+    : Node("minimal_publisher"), count_(0)
+    {
+      rclcpp::QoS qos_profile(10);
+      declare_parameter<int>("hz", 10);
+      int hz = get_parameter("hz").as_int();
+      std::chrono::milliseconds timer_period{static_cast<int>(1.0 / hz * 1000)};
+      std::chrono::milliseconds lease_duration{static_cast<int>(1.0 / hz * 1000 * 1.1)};
+      qos_profile.liveliness(RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC)
+            .liveliness_lease_duration(lease_duration)
+            .deadline(lease_duration);
+      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", qos_profile);
+      timer_ = this->create_wall_timer(timer_period, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
