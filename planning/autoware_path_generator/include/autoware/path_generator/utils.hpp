ed47ecbb1f0fe49216b3cb91c8cfcc21efcb89c2
mitukou1109 <mitukou1109@gmail.com>
2024-12-23 17:29:29 +0900

    add path_generator package
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    fix spell check error
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    include necessary headers
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    change package version to 0.0.0
    
    Co-authored-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    fix include guard name
    
    Co-authored-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    replace flowchart uml with pre-generated image
    
    Co-authored-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    style(pre-commit): autofix
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    replace tier4_planning_msgs with autoware_internal_planning_msgs
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    style(pre-commit): autofix
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    use LaneletSequence instead of ConstLanelets
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    set orientation to path points
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    crop path bound to fit trajectory
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    offset path bound
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    no need to make return value optional
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    address deprecation warning
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    add doxygen comments
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    support multiple previous/next lanelets
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    fix path bound cut issue
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    group parameters
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    add turn signal activation feature
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    fix turn direction check process
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    consider required end point
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    keep turn signal activated until reaching desired end point if without conflicts
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>
    
    add missing parameters
    
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>

--- planning/autoware_path_generator/include/autoware/path_generator/utils.hpp
+++ planning/autoware_path_generator/include/autoware/path_generator/utils.hpp
@@ -18,6 +18,7 @@
 #include "autoware/path_generator/common_structs.hpp"
 
 #include <autoware_internal_planning_msgs/msg/path_with_lane_id.hpp>
+#include <autoware_vehicle_msgs/msg/turn_indicators_command.hpp>
 
 #include <optional>
 #include <utility>
@@ -27,6 +28,12 @@ namespace autoware::path_generator
 {
 using autoware_internal_planning_msgs::msg::PathPointWithLaneId;
 using autoware_internal_planning_msgs::msg::PathWithLaneId;
+using autoware_vehicle_msgs::msg::TurnIndicatorsCommand;
+
+const std::unordered_map<std::string, uint8_t> turn_signal_command_map = {
+  {"left", TurnIndicatorsCommand::ENABLE_LEFT},
+  {"right", TurnIndicatorsCommand::ENABLE_RIGHT},
+  {"straight", TurnIndicatorsCommand::DISABLE}};
 
 namespace utils
 {
@@ -113,6 +120,36 @@ std::vector<geometry_msgs::msg::Point> get_path_bound(
   const lanelet::CompoundLineString2d & lanelet_bound,
   const lanelet::CompoundLineString2d & lanelet_centerline, const double s_start,
   const double s_end);
+
+/**
+ * @brief get earliest turn signal based on turn direction specified for lanelets
+ * @param path target path
+ * @param planner_data planner data
+ * @param current_pose current pose of ego vehicle
+ * @param current_vel current longitudinal velocity of ego vehicle
+ * @param search_distance base search distance
+ * @param search_time time to extend search distance
+ * @param resampling_interval resampling interval for required end point determination
+ * @param angle_threshold_deg angle threshold for required end point determination
+ * @param base_link_to_front distance from base link to front of ego vehicle
+ * @return turn signal
+ */
+TurnIndicatorsCommand get_turn_signal(
+  const PathWithLaneId & path, const PlannerData & planner_data,
+  const geometry_msgs::msg::Pose & current_pose, const double current_vel,
+  const double search_distance, const double search_time, const double resampling_interval,
+  const double angle_threshold_deg, const double base_link_to_front);
+
+/**
+ * @brief get required end point for turn signal activation
+ * @param lanelet target lanelet
+ * @param resampling_interval centerline resampling interval
+ * @param angle_threshold_deg  yaw angle difference threshold
+ * @return required end point
+ */
+lanelet::ConstPoint2d get_turn_signal_required_end_point(
+  const lanelet::ConstLanelet & lanelet, const double resampling_interval,
+  const double angle_threshold_deg);
 }  // namespace utils
 }  // namespace autoware::path_generator
 
