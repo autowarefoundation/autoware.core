2024e7f0eb1c5c5f8e06d09c0797ff4f8d2c5091
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-02-20 14:15:58 +0900

    Apply suggestions from code review

--- testing/autoware_test_utils/src/topic_snapshot_saver.cpp
+++ testing/autoware_test_utils/src/topic_snapshot_saver.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <autoware_test_utils/autoware_test_utils.hpp>
-#include <autoware_test_utils/mock_data_parser.hpp>
+#include "autoware_test_utils/autoware_test_utils.hpp"
+#include "autoware_test_utils/mock_data_parser.hpp"
 #include <rclcpp/logger.hpp>
 #include <rclcpp/rclcpp.hpp>
 
