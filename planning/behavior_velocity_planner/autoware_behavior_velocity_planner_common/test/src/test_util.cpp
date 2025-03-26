2b925ee507fb14f52920f8bdd296a8284a1071a8
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-03-26 13:35:34 +0900

    Update planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/test/src/test_util.cpp

--- planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/test/src/test_util.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_planner_common/test/src/test_util.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "./utils.hpp"
+#include "utils.hpp"
 #include "autoware/behavior_velocity_planner_common/utilization/util.hpp"
 #include "autoware_test_utils/autoware_test_utils.hpp"
 
